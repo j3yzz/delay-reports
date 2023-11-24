a427875f58bfcfd7d9446f4fe26c8de8137b9aa9
Amirreza Kheradmand <amir.rezakheradmand.2017@gmail.com>
2023-11-25 00:28:38 +0330

    feat(seeder): add database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,14 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Containers\Agent\Models\Agent;
+use App\Containers\Delivery\Models\Order;
+use App\Containers\Delivery\Models\Trip;
+use App\Containers\Driver\Models\Driver;
+use App\Containers\User\Models\User;
+use App\Containers\Vendor\Models\Vendor;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +19,74 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        if (User::query()->count() == 0) {
+            User::query()->create([
+                'name' => fake()->name,
+                'phone_number' => '09123456789',
+                'phone_number_verified_at' => now()->subDays(5),
+                'password' => Hash::make('123123123'),
+            ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            User::query()->create([
+                'name' => fake()->name,
+                'phone_number' => '09112345678',
+                'phone_number_verified_at' => now()->subDays(10),
+                'password' => Hash::make('password'),
+            ]);
+        }
+
+        if (Vendor::query()->count() == 0) {
+            for ($i = 0; $i < 20; $i++) {
+                Vendor::query()->insertOrIgnore([
+                    'name' => fake()->lastName,
+                    'phone_number' => fake()->phoneNumber,
+                    'address' => fake()->address,
+                ]);
+            }
+        }
+
+        if (Driver::query()->count() == 0) {
+            for ($i = 0; $i < 5; $i++) {
+                Driver::query()->insertOrIgnore([
+                    'name' => fake()->name,
+                    'phone_number' => fake()->phoneNumber,
+                ]);
+            }
+        }
+
+        if (Agent::query()->count() == 0) {
+            for ($i = 0; $i < 5; $i++) {
+                Agent::query()->insertOrIgnore([
+                    'name' => fake()->name,
+                    'phone_number' => fake()->phoneNumber,
+                ]);
+            }
+        }
+
+        if (Order::query()->count() == 0) {
+            $firstUser = User::query()->first();
+
+            Order::query()->create([
+                'user_id' => $firstUser->id,
+                'vendor_id' => Vendor::query()->find(1)->id,
+                'ordered_at' => now()->subDays(10),
+                'delivery_time' => rand(10, 50),
+                'status' => Order::STATUS_DELIVERED,
+            ]);
+
+            $order = Order::query()->create([
+                'user_id' => $firstUser->id,
+                'vendor_id' => Vendor::query()->find(2)->id,
+                'ordered_at' => now()->subMinutes(41),
+                'delivery_time' => 40,
+                'status' => Order::STATUS_PENDING,
+            ]);
+
+            Trip::query()->create([
+                'order_id' => $order->id,
+                'driver_id' => Driver::query()->find(1)->id,
+                'status' => Trip::STATUS_AT_VENDOR,
+            ]);
+        }
     }
 }
