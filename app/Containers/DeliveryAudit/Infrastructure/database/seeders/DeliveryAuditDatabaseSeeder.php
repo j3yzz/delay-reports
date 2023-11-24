1e8a42d7033c4dc1be4eb92427fd7a0d2a51a222
Amirreza Kheradmand <amir.rezakheradmand.2017@gmail.com>
2023-11-25 00:00:56 +0330

    feat: add Delivery Audit module.

---
+++ app/Containers/DeliveryAudit/Infrastructure/database/seeders/DeliveryAuditDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Containers\DeliveryAudit\database\seeders;
+
+use Illuminate\Database\Seeder;
+
+class DeliveryAuditDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ app/Containers/DeliveryAudit/Providers/RouteServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Containers\DeliveryAudit\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * The module namespace to assume when generating URLs to actions.
+     */
+    protected string $moduleNamespace = 'App\Containers\DeliveryAudit\Http\Controllers';
+
+    /**
+     * Called before routes are registered.
+     *
+     * Register any model bindings or pattern based filters.
+     */
+    public function boot(): void
+    {
+        parent::boot();
+    }
+
+    /**
+     * Define the routes for the application.
+     */
+    public function map(): void
+    {
+        $this->mapApiRoutes();
+    }
+
+    /**
+     * Define the "api" routes for the application.
+     *
+     * These routes are typically stateless.
+     */
+    protected function mapApiRoutes(): void
+    {
+        Route::prefix('api')
+            ->middleware('api')
+            ->namespace($this->moduleNamespace)
+            ->group(module_path('DeliveryAudit', '/Routes/api.php'));
+    }
+}
