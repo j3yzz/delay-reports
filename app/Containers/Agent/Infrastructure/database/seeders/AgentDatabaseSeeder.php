bced20327d6efb01b275724ad5374361ec02048c
Amirreza Kheradmand <amir.rezakheradmand.2017@gmail.com>
2023-11-24 20:56:34 +0330

    add: add Agent module

---
+++ app/Containers/Agent/Infrastructure/database/seeders/AgentDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Containers\Agent\database\seeders;
+
+use Illuminate\Database\Seeder;
+
+class AgentDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
